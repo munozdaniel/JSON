2ca29cdb172c479890b88356c6c5d2febb2ed7c1
munozdaniel <munozda87@hotmail.com>
2014-12-19 08:04:26 -0300

    MainActivity: Comentando codigo.

--- app/src/main/java/type/proy/com/json/MainActivity.java
+++ app/src/main/java/type/proy/com/json/MainActivity.java
@@ -25,91 +25,7 @@ import java.io.InputStreamReader;
 
 
 public class MainActivity extends ActionBarActivity {
-    /**
-     *
-     */
-    public String readJSONFeed(String URL) {
-        StringBuilder stringBuilder = new StringBuilder();
-        HttpClient httpClient = new DefaultHttpClient();
-        //Para conectarse a un servicio web, la aplicación necesita en primer lugar conectarse al servidor utilizando HTTP
-        HttpGet httpGet = new HttpGet(URL);
-        try {
-            HttpResponse response = httpClient.execute(httpGet);
-            StatusLine statusLine = response.getStatusLine();
-            int statusCode = statusLine.getStatusCode();
-            if (statusCode == 200) {
-                HttpEntity entity = response.getEntity();
-                InputStream inputStream = entity.getContent();
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(inputStream));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    stringBuilder.append(line);
-                }
-                inputStream.close();
-            } else {
-                Log.d("JSON", "Failed to download file");
-            }
-        } catch (Exception e) {
-            Log.d("readJSONFeed", e.getLocalizedMessage());
-        }
-        return stringBuilder.toString();
-    }
-    /*****************************************************************/
 
-    private class ReadWeatherJSONFeedTask extends AsyncTask<String, Void, String> {
-        protected String doInBackground(String... urls) {
-            return readJSONFeed(urls[0]);
-        }
-
-        protected void onPostExecute(String result) {
-            try {
-                JSONObject jsonObject = new JSONObject(result);
-                JSONObject weatherObservationItems =
-                        new JSONObject(jsonObject.getString("weatherObservation"));
-
-                Toast.makeText(getBaseContext(),
-                        weatherObservationItems.getString("clouds") +
-                                " - " + weatherObservationItems.getString("stationName"),
-                        Toast.LENGTH_SHORT).show();
-            } catch (Exception e) {
-                Log.d("ReadWeatherJSONFeedTask", e.getLocalizedMessage());
-            }
-        }
-    }
-
-
-    /*****************************************************************/
-    private class ReadPlacesFeedTask extends AsyncTask
-            <String, Void, String> {
-        protected String doInBackground(String... urls) {
-            return readJSONFeed(urls[0]);
-        }
-
-        protected void onPostExecute(String result) {
-            try {
-                JSONObject jsonObject = new JSONObject(result);
-                JSONArray postalCodesItems = new
-                        JSONArray(jsonObject.getString("postalCodes"));
-
-                //---print out the content of the json feed---
-                for (int i = 0; i < postalCodesItems.length(); i++) {
-                    JSONObject postalCodesItem =
-                            postalCodesItems.getJSONObject(i);
-                    Toast.makeText(getBaseContext(),
-                            postalCodesItem.getString("postalCode") + " - " +
-                                    postalCodesItem.getString("placeName") + ", " +
-                                    postalCodesItem.getString("countryCode"),
-                            Toast.LENGTH_SHORT).show();
-                   // Toast.makeText(getBaseContext(),"Vueltaaaaaa",Toast.LENGTH_SHORT).show();
-                }
-            } catch (Exception e) {
-                Log.d("ReadPlacesFeedTask", e.getLocalizedMessage());
-            }
-        }
-    }
-
-    /*****************************************************************/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -139,7 +55,9 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    /*****************************************************************/
+    /**
+     * 1.
+     * */
 
     public void btnGetWeather(View view) {
         EditText txtLat = (EditText) findViewById(R.id.txtLatitud);
@@ -150,7 +68,9 @@ public class MainActivity extends ActionBarActivity {
                         txtLat.getEditableText().toString() + "&lng=" +
                         txtLong.getText().toString());
     }
-    /*****************************************************************/
+    /**
+     * 1.
+     * */
     public void btnGetPlaces(View view) {
         EditText txtPostalCode = (EditText) findViewById(R.id.txtPostalCode);
         new ReadPlacesFeedTask().execute(
@@ -159,4 +79,109 @@ public class MainActivity extends ActionBarActivity {
                         "&maxRows=10&username=demo");
     }
     /*****************************************************************/
+    /**
+     * 2. Clase anidada. Realiza tareas asincronicas.
+     *
+     *
+     * */
+    private class ReadWeatherJSONFeedTask extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... urls) {
+            return readJSONFeed(urls[0]);//Enviamos los datos por GET.
+        }
+
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONObject weatherObservationItems =
+                        new JSONObject(jsonObject.getString("weatherObservation"));
+
+                Toast.makeText(getBaseContext(),
+                        weatherObservationItems.getString("clouds") +
+                                " - " + weatherObservationItems.getString("stationName"),
+                        Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Log.d("ReadWeatherJSONFeedTask", e.getLocalizedMessage());
+            }
+        }
+    }
+
+    /**
+     * 2.
+     * */
+    private class ReadPlacesFeedTask extends AsyncTask
+            <String, Void, String> {
+        protected String doInBackground(String... urls) {
+            return readJSONFeed(urls[0]);
+        }
+
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray postalCodesItems = new
+                        JSONArray(jsonObject.getString("postalCodes"));
+
+                //---print out the content of the json feed---
+                for (int i = 0; i < postalCodesItems.length(); i++) {
+                    JSONObject postalCodesItem =
+                            postalCodesItems.getJSONObject(i);
+                    Toast.makeText(getBaseContext(),
+                            postalCodesItem.getString("postalCode") + " - " +
+                                    postalCodesItem.getString("placeName") + ", " +
+                                    postalCodesItem.getString("countryCode"),
+                            Toast.LENGTH_SHORT).show();
+                    // Toast.makeText(getBaseContext(),"Vueltaaaaaa",Toast.LENGTH_SHORT).show();
+                }
+            } catch (Exception e) {
+                Log.d("ReadPlacesFeedTask", e.getLocalizedMessage());
+            }
+        }
+    }
+
+    /*****************************************************************/
+    /**
+     * 3.
+     * URL WHEATER: http://ws.geonames.org/findNearByWeatherJSON?lat=37.77493&lng=-122.419416
+     * URL PLACES: http://api.geonames.org/postalCodeSearchJSON?postalcode=89118&maxRows=10&username=demo
+     */
+    public String readJSONFeed(String URL) {
+        StringBuilder stringBuilder = new StringBuilder();
+        HttpClient httpClient = new DefaultHttpClient();
+        //Para conectarse a un servicio web, la aplicación necesita en primer lugar conectarse al servidor utilizando HTTP
+        HttpGet httpGet = new HttpGet(URL);
+
+        try {
+            //Es necesario determinar también si usted va a utilizar HTTP GET o HTTP POST.
+            HttpResponse response = httpClient.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if (statusCode == 200) {
+                HttpEntity entity = response.getEntity();
+                InputStream inputStream = entity.getContent();
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                inputStream.close();
+            } else {
+                Log.d("JSON", "Failed to download file");
+            }
+        } catch (Exception e) {
+            Log.d("readJSONFeed", e.getLocalizedMessage());
+        }
+        //Log.d("JSON", "************ RETORNO **************");
+        //Log.d("JSON", stringBuilder.toString());
+        // Log.d("JSON", "************ FIN RETORNO **************");
+
+        /* {"postalCodes":[{"adminCode2":"003","adminCode1":"NV","adminName2":"Clark","lng":-115.216856,"countryCode":"US",
+        "postalCode":"89118","adminName1":"Nevada","placeName":"Las Vegas","lat":36.081052},{"adminCode2":"2284","adminCode1":"Y",
+        "adminName2":"Örnsköldsvik","lng":18.71525287628174,"countryCode":"SE","postalCode":"891 18","adminName1":"Västernorrland",
+        "placeName":"Örnsköldsvik","lat":63.290913995082185},{"adminCode2":"28038","adminName3":"Tampico","adminCode1":"TAM",
+        "adminName2":"Tampico","lng":-98.81768,"countryCode":"MX","postalCode":"89118","adminName1":"Tamaulipas",
+        "placeName":"La Florida","lat":24.910608}]}
+        */
+        return stringBuilder.toString();//Devuelve los datos recuperados en formato JSON.
+    }
+
 }
